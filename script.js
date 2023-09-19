c26aef0f0d2af529f428581b77d297f77705f632
nguyen-dat-1612 <nguyendatpvdpqn@gmail.com>
2023-09-19 09:35:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//hamburget menu
+
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function () {
+    navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('active');
+}
